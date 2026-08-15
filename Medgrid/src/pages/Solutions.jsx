d9aa5e10912f3087596a7db75323b66ba8218c49
Plangji Solomon Dawan <plangjidawan@gmail.com>
2026-08-15 22:17:53 +0100

    feat: update Solutions component with core Medgrid solution details

--- Medgrid/src/pages/Solutions.jsx
+++ Medgrid/src/pages/Solutions.jsx
@@ -1,6 +1,14 @@
 function Solutions(){
   return(
-    <h1>Solutions Page</h1>
+    <div className="w-full">
+      <div className="mx-auto max-w-4xl text-center" >
+        <h1 className="text-2xl font-bold text-black-700 sm:text-3xl md:text-4xl">Our Core Med<span className="text-[rgb(80,170,141)]">Grid</span> Soloution</h1>
+        <p className="mt-4 px-2 text-sm leading-6 text-gray-800 sm:px-6 sm:text-base md:px-10 md:text-lg lg:px-16 lg:text-xl"> Leveraging data and technology to optimize the healthcare ecosystem</p>
+        <p className="mt-4 px-2 text-sm leading-6 text-gray-800 sm:px-6 sm:text-base md:px-10 md:text-lg lg:px-16 lg:text-xl">across Nigeria.</p>
+      </div>
+
+    </div>
+    
   )
 }
 export default Solutions;
